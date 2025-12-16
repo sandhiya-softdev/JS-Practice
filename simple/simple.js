236ecbbef8c8e38d2a5c4e9b989c3e9cceb3148a
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-16 22:10:19 +0530

    JS-Practice commit

--- simple/simple.js
+++ simple/simple.js
@@ -1,5 +1,23 @@
-var things=document.getElementById("on");
+
+// var things=document.getElementById("hii");
+// var things1=document.getElementById("on");
+
 
 function fname(){
-    var things=document.getElementById("on")
+var things1=document.getElementById("on");
+things1.style.display="block";
+
+}
+function fnamehide(){
+var things1=document.getElementById("on");
+things1.style.display="none";
+
+}
+function high(){
+    var greet=document.getElementById("on1")
+    greet.style.display="block";
+}
+function highhide(){
+    var greet=document.getElementById("on1")
+    greet.style.display="none";
 }
