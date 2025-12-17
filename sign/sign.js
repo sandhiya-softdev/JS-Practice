a8540868299017c0e8ba8f1ec3862c7defea64c4
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-17 13:05:17 +0530

    JS-Practice commit

---
+++ sign/sign.js
@@ -0,0 +1,13 @@
+function greet(){
+var apple1=Number(document.getElementById("on1").value);
+if(apple1>4){
+    alert("number is requried")
+}else{
+    alert("there is not required")
+}
+
+
+
+    
+}
+var apple2=document.getElementById("on2");
