258869651660e6b45ee79f1c8c62fd35fc3d9ecf
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-20 00:08:56 +0530

    JS-Practice commit

---
+++ array7.js
@@ -0,0 +1,13 @@
+// let a = [10, 20, 30];
+// a.reverse();
+// console.log(a);
+
+// let names = ["ram", "sam", "tom"];
+// names.reverse();
+// console.log(names);
+
+// let dude = ["good","bad","fine"];
+// dude.reverse();
+// console.log(dude);
+
+
