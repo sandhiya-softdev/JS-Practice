924b90b2f56b8b0dbd5ee046cd30826d38ec9d74
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-19 21:51:14 +0530

    JS-Practice commit

--- array3.js
+++ array3.js
@@ -6,3 +6,32 @@
 // a.unshift("x");
 // console.log(a);
 
+// let a=["b","c"];
+// let x=a.unshift("a");
+// console.log(x);
+
+// let a = ["b","c"];
+// let x = a.unshift("a");
+// console.log(a.length);
+
+// let a = ["c"];
+// a.unshift("b");
+// a.unshift("a");
+// console.log(a);
+
+
+// let a = ["c"];
+// let x = a.unshift("b","a");
+// console.log(a, x);
+
+
+// let a = ["b","c"];
+// a.unshift("a");
+// a.pop();
+// console.log(a);
+
+
+
+
+
+
