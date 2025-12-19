da36cbd9333000803a90966901e23c5afd914ce9
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-19 11:16:14 +0530

    JS-Practice commit

---
+++ simple.js
@@ -0,0 +1,14 @@
+// async function myFun(){
+//     return 1;
+// }
+// console.log(myFun());
+
+// async function always return promise
+
+
+let thinking = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+
+    },1000)
+
+})
