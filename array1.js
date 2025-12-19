9364eb43e971cabc4c542b12334dd4d70b568b1a
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-19 17:43:10 +0530

    JS-Practice commit

--- array1.js
+++ array1.js
@@ -1,6 +1,28 @@
 // what is array methods push
 // in array push it is last items addd
 
-let empty = [];
-empty.push(1,2,3);
-console.log(empty);
+// let empty = [];
+// empty.push(1,2,3);
+// console.log(empty);
+
+
+// let fruits =["apple","banana"];
+// fruits.push("orange");
+// console.log(fruits);
+
+// let colors= ["red"];
+// colors.push("bkue","green");
+// console.log(colors);
+
+// let numbers =[];
+// for(i=1;i<=5;i++){
+//     numbers.push(i);
+// }
+// console.log(numbers);
+
+
+
+
+
+
+
