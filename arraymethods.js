b70cafb435b296ccbbde3795ccb4bf0e9a0d06b1
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-19 17:15:36 +0530

    JS-Practice commit

--- arraymethods.js
+++ arraymethods.js
@@ -1,5 +1,9 @@
-let s = [0,1,2,3];
- for(i=0;i<s.length;i++){
-    console.log(s[i]);
+// let s = [0,1,2,3];
+//  for(i=0;i<s.length;i++){
+//     console.log(s[i]);
     
-}
+// }
+
+let things = ["fan", "fridge","washmachine"];
+console.log(things);
+
