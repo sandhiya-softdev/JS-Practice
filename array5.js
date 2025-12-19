c810de58264039ddb3d24b935cbe27ffe58ba465
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-19 23:19:47 +0530

    JS-Practice commit

--- array5.js
+++ array5.js
@@ -2,3 +2,25 @@
 // oppo.splice(0,1);
 // console.log(oppo);
 
+
+// let laptop = ["vp","wipro","samsung"];
+// laptop.splice(1,2,"tcs");
+// console.log(laptop);
+
+// let pen =["a","b","c","d"];
+// pen.splice(1,1);
+// console.log(pen);
+
+// let books = ["a","b","c","d"];
+// books.splice(0,2);
+// console.log(books);
+
+let mobile = ["a","d"];
+mobile.splice(1,0,"b","c");
+console.log(mobile);
+
+
+
+
+
+
