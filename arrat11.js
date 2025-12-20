627989dbb183f8af93d11c2fe9f287632da6e5dd
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-20 08:23:10 +0530

    JS-Practice commit

--- arrat11.js
+++
@@ -1,6 +0,0 @@
-// let a= Array.of(2,"hello",true);
-// console.log(a);
-
-
-let b = Array.of(5);
-console.log(b);
---
+++ array11.js
@@ -0,0 +1,8 @@
+// let p = Array.of(10, 20);
+// let q = Array.of(10);
+
+// console.log(p);
+// console.log(q);
+
+let d = Array.of(1, "hi", true);
+console.log(d);
