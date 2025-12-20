314e3eed39c3cebe7cff276c431df8a017775e21
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-20 11:51:22 +0530

    JS-Practice commit

--- array17.js
+++ array17.js
@@ -1,3 +1,8 @@
-let arr = [5, 1];
-arr.sort((a, b) => a - b);
-console.log(arr);
+// let arr = [5, 1];
+// arr.sort((a, b) => a - b);
+// console.log(arr);
+
+// let arr=[1,3,5,2,4,7,8];
+// arr.sort();
+// console.log(arr);
+
