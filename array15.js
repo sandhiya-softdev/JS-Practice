f41f2b8ba24567887e92701bb3c6dcdfedd66078
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-22 23:10:35 +0530

    JS-Practice commit

--- array15.js
+++ array15.js
@@ -1,6 +1,6 @@
 // let a =[1,2,3,4,4,5,6,7,8,];
 // console.log(a.lastIndexOf(4));
 
-// let text = "javascript";
+let text = "javascript";
 
-// console.log(text.lastIndexOf("a"));
+console.log(text.lastIndexOf("a"));
