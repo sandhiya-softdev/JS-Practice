16fde4596b83df7759502e7d875beac753a736b9
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-22 23:10:05 +0530

    JS-Practice commit

--- array14.js
+++ array14.js
@@ -1,6 +1,6 @@
 // let a =[1,"Hello","there"];
 // console.log(a.includes(1));
 
-// let a =["Hello","there"];
-// console.log(a.includes(1));
+let a =["Hello","there"];
+console.log(a.includes(1));
 
