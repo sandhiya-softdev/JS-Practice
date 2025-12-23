9ccea6e7f6f1dc81d8a53cf26ac2c3816fdeae06
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-23 22:22:03 +0530

    JS-Practice commit

--- array23.js
+++ array23.js
@@ -4,3 +4,23 @@
 
 // });
 // console.log(b);
+
+// let a = [2,4,6,8];
+// let b=a.every(function(hello){
+//     return hello >= 2;
+
+
+// });
+// console.log(b);
+
+// let a = [10,20,30];
+// let b=a.every(function(high){
+//     return high > 5;
+
+// });
+// console.log(b);
+
+
+
+
+
