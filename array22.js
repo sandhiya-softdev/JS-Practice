79b94d5fef409cd94b41ff4956d5b816c1a8e9ee
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-23 19:14:01 +0530

    JS-Practice commit

--- array22.js
+++ array22.js
@@ -1,29 +1,17 @@
-// let x = [1,2,3,4,5];
-// let y=x.map(function(num){
-//     return x+10;
-
-const { useLayoutEffect } = require("react");
-
-// });
-// console.log(y);
-
-// let a = [1,2,3,4];
-// let b=a.map(function(num) {
-// return num+2;
+// let a = [1,2,3,4,5];
+// let b=a.map(function(num){
+//     return num + 3;
 
 // });
 // console.log(b);
 
-// let a = [1,2,3,4,5,6,7,8,9,10];
-// let b= a.map(function(hello) {
-//    return hello+4;
+// practice
+let nums = [1,2,3,4];
+let add=nums.map(function (high){
+    return high +2;
     
-// });
-// console.log(b);
-
-    
-
-
+})
+console.log(add);
 
 
 
