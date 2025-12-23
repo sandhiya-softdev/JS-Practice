b697b304627d628c2fc775c4445ddb80cf09e9f0
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-23 19:25:03 +0530

    JS-Practice commit

--- array22.js
+++ array22.js
@@ -6,12 +6,33 @@
 // console.log(b);
 
 // practice
-let nums = [1,2,3,4];
-let add=nums.map(function (high){
-    return high +2;
+// let nums = [1,2,3,4];
+// let add=nums.map(function (high){
+//     return high +2;
     
-})
-console.log(add);
+// })
+// console.log(add);
+
+
+// let nums = [5,10,15];
+// let high=nums.map(function(hello){
+//     return hello*10;
+    
+// });
+// console.log(high);
+
+// let nums = [1,2,3];
+// let a=nums.map(function(hello){
+//     return String(hello);
+
+// });
+// console.log(a);
+
+
+
+
+
+
 
 
 
