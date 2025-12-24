01cb7312b1141ab90bc50ce1ed89757cc978a3f3
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-24 07:08:46 +0530

    JS-Practice commit

--- array26.js
+++ array26.js
@@ -3,4 +3,15 @@
 //     return hello > 10;
 
 // });
-// console.log(x);
+// console.log(x);00000
+
+// let nums = [2,3,6,7,8];
+// h=nums.find(function(opposite){
+//     if(opposite%2!==0) {
+//         return true;
+//     }else{
+//         return false;
+//     }
+
+// });
+// console.log(h);
