79a976e653204cc4742885f9366b57318bef94d4
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-24 07:32:48 +0530

    JS-Practice commit

---
+++ array27.js
@@ -0,0 +1,13 @@
+// let x =[1,2,3,4,5];
+// let y=x.findIndex(function(hey){
+//     return hey===1;
+
+// });
+// console.log(y);
+
+
+// let nums =[1,3,4,7,9];
+// let y=nums.findIndex(function(hello){
+//     return hello%2===0;
+// });
+// console.log(y);
