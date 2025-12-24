1868f56f86f32d07b39dca4254737495ab9882fa
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-24 07:28:20 +0530

    JS-Practice commit

--- array26.js
+++ array26.js
@@ -15,3 +15,41 @@
 
 // });
 // console.log(h);
+
+
+// let things = ["hi","hello","bye"];
+// let occur = things.find(function(well){
+//     return well.length >3;
+
+// });
+// console.log(occur);
+
+
+// let hii = ["cat","dog","cow"];
+// let hey=hii.find(function(there){
+//     return there[0]==="c";
+
+// });
+// console.log(hey);
+
+
+// let there = [2,4,6,8];
+// let hey=there.find(function(hello){
+//     return hello%2 !== 0;
+
+// });
+// console.log(hey);
+
+// let people = [
+//   { name: "Ram", age: 12 },
+//   { name: "Sam", age: 18 },
+//   { name: "Tom", age: 20 }
+// ];
+// let x = people.find(function(hey){
+//     return hey.age >=18;
+// });
+// console.log(x);
+
+
+
+
