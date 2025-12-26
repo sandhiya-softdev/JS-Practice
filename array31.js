a2bdb4667988fd440c174bce943de319f0c74fa6
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-26 23:24:17 +0530

    JS-Practice commit

--- array31.js
+++ array31.js
@@ -2,7 +2,7 @@
 // let b=a.flat();
 // console.log(b);
 
-// let a =[1[2,3],[4],[5,6],[7,8,9]];
-// let b= a.flat();
-// console.log(b);
+let a =[1[2,3],[4],[5,6],[7,8,9]];
+let b= a.flat();
+console.log(b);
 
