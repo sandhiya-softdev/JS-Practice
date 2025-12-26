0ac2c8b2d6541e016990f5f32c91d96e5d3e4bed
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-26 23:26:45 +0530

    JS-Practice commit

--- array19.js
+++ array19.js
@@ -2,13 +2,13 @@
 // a.fill(9);
 // console.log(a);
 
-// let x = [2,4,6,7,8,2,5,6];
-// x.fill(1,0,5);
-// console.log(x);
+let x = [2,4,6,7,8,2,5,6];
+x.fill(1,0,5);
+console.log(x);
 
-let b = [1, 2, 3, 4, 5];
-b.fill(9, 1, 4);
+// let b = [1, 2, 3, 4, 5];
+// b.fill(9, 1, 4);
 
-console.log(b);
+// console.log(b);
 
 
