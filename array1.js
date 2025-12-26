6be7df67cfd3cf21004ca4fbd8b4d160da74a0b9
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-26 23:28:43 +0530

    JS-Practice commit

--- array1.js
+++ array1.js
@@ -39,9 +39,9 @@
 // let a=[1,2,3];
 // console.log(a.length);
 
-// let arr = ["x","y"];
-// arr.push("z");
-// console.log(arr.length);
+let arr = ["x","y"];
+arr.push("z");
+console.log(arr.length);
 
 // let hello = ["ram","please","never come"];
 // hello.push("hate you");
