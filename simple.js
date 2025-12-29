591065bbd6eac79d8a320fb9d934570e0c0fc729
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-29 22:55:32 +0530

    JS-Practice commit

--- simple.js
+++ simple.js
@@ -23,6 +23,20 @@
 
 
 
-let x= 5;
-let y=10;
-console.log(x+y*2);
+// let x= 5;
+// let y=10;
+// console.log(x+y*2);
+
+// let a = 2;
+// let b="5";
+// console.log(a*b);
+
+
+const arr = [1,2,3,4,5];
+
+for(var i=0;i<arr.length;i++){
+    setTimeout(function(){
+        console.log(i);
+        
+    },1000)
+}
