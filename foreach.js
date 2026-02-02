fdb947f953cfb004920b92494802c2a0129b1c10
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2026-02-02 16:12:04 +0530

    JS-Practice commit

---
+++ foreach.js
@@ -0,0 +1,34 @@
+// let hello = [1,2,3,4,5,6];
+// hello.forEach(function(hey){
+//     console.log(hey);
+    
+// });
+
+
+// let x = [10,20,30];
+// x.forEach(function(cursed){
+//     cursed+2;
+//     console.log(cursed);
+    
+
+// });
+
+
+// let x = ["apple","banana"];
+// x.forEach(function(hii){
+//     hii + "i Like";
+//     console.log(hii);
+    
+
+// });
+
+// foreach na array la irukkurah one by one values ah etukka ah use aguthu like loop mathiri
+
+// syntax
+
+// array.foreach(function(parameter){
+// })
+
+const nums = [1,2,34]
+
+
