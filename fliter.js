a262293849eb7d9d3e2c4f8436f8a8722806001b
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2026-02-16 17:23:12 +0530

    JS-Practice commit

---
+++ fliter.js
@@ -0,0 +1,57 @@
+// let num = [2,7,2,9,1];
+// let hello = num.filter(function(hey){
+//     return hey >5;
+
+// });
+// console.log(hello);
+
+// let x = ["cat", "lion", "tiger", "dog"];
+// let y=x.filter(function(hello){
+//     return hello.length>3;
+
+// });
+// console.log(y);
+
+// let x = [2, 5, 6, 7, 8];
+// let y = x.filter(function(hey){
+//     return hey>5;
+// });
+// console.log(y);
+
+    // let products = [
+    //   { name: "Shirt", price: 500 },
+    //   { name: "Pant", price: 1500 },
+    //   { name: "Shoes", price: 2500 },
+    //   { name: "Cap", price: 300 }
+    // ];
+
+
+    //  let hey=products.filter((m)=>{
+    //     return m.price>1000;
+    // })
+    // console.log(hey);
+    
+
+//     let package = [
+//         {name:"pen",amount:10},
+//         {name:"book",amount:200},
+//         {name:"bag",amount:900}
+//     ];
+
+//    const hey= package.filter((m)=>{
+//        return m.amount<500;
+//     })
+// console.log(hey);
+
+
+// let students = [
+//     {name:"arun",average:"pass"},
+//     {name:"bala",average:"fail"},
+//     {name:"chithra",average:"pass"},
+// ];
+// const hey=students.filter((m)=>{
+//     return m.average===pass;
+// })
+// console.log(hey);
+
+
